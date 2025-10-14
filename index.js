0223e8c5cf39cccd74f50012e998514b18c69075
Jobaer Afroz Jemin <139230529+Jobaer-afroz-jemin-029@users.noreply.github.com>
2025-10-15 02:18:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Gmail App Password
+    user: '22235103029@cse.bubt.edu.bd'
+    pass: 'ntxjimhiqnsvipkn', // Gmail App Password
   },
 });
 
